494548a291b47abc9a9c0ff9d3a443a4ea922e15
Yuriamani <yuriamani7@gmail.com>
2025-03-04 19:03:34 +0300

    added settings and analytics page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import OverviewPage from "./pages/OverviewPage"
 import ProductsPage from "./pages/ProductsPage"
 import UsersPage from "./pages/UsersPage"
 import SalesPage from "./pages/SalesPage"
-import Sidebar from "./components/Sidebar";
+import OrdersPage from "./pages/OrdersPage"
+import Sidebar from "./components/common/Sidebar";
+import AnalyticsPage from "./pages/AnalyticsPage";
+import SettingsPage from "./pages/SettingsPage";
 
 function App() {
 
@@ -22,6 +25,9 @@ function App() {
         <Route path="/products" element={<ProductsPage/>} />
         <Route path="/users" element={<UsersPage/>} />
         <Route path="/sales" element={<SalesPage/>} />
+        <Route path="/orders" element={<OrdersPage/>} />
+        <Route path="/analytics" element={<AnalyticsPage/>} />
+        <Route path="/settings" element={<SettingsPage/>} />
       </Routes>
     </div>
     
